785c1e8a568c1d5df5be1d72f4f7eb86548649fd
Bohdan <bodya.ru02@gmail.com>
2022-04-22 19:39:32 +0300

    Works for 1 level of zip files, pushes a poison pill too early when there are many nestend archives

--- src/archives.rs
+++ src/archives.rs
@@ -1,52 +1,55 @@
-use std::{io::{Read, Cursor}, path::Path, collections::VecDeque};
+use std::{io::{Read, Cursor}, path::{Path, PathBuf}, collections::VecDeque, rc::Rc, cell::RefCell};
 
-use zip::ZipArchive;
+use zip::{ZipArchive};
 
 use crate::mt::{size_limits::FILE_SIZE_LIMIT_BYTES, MtDeque};
+use crate::list_and_read::FileForIndex;
 
-pub fn unzip_from_memory(path: &str, mt_d_file_contents: &MtDeque<Option<String>>) {
-    let file = std::fs::read(path).unwrap();
-    let mut zip_archives_deque: VecDeque<ZipArchive<Cursor<Vec<u8>>>> = VecDeque::new();
-    zip_archives_deque.push_back(ZipArchive::new(Cursor::new(file)).unwrap());
-    
-    loop {
-        let mut zip_archive = match zip_archives_deque.pop_front() {
-            Some(v) => v,
-            None => break,
+pub fn get_contents_from_zip_file(path: &str, zip_archive: RefCell<ZipArchive<Cursor<Vec<u8>>>>, mt_d_filenames: &MtDeque<Option<FileForIndex>>, mt_d_file_contents: &MtDeque<Option<String>>) {
+    let mut zip_archive_borrow = zip_archive.borrow_mut();
+    let mut zip_file =  zip_archive_borrow.by_name(path).unwrap();
+
+    if zip_file.is_dir() {
+        return;
+    }
+
+    if zip_file.size() >= FILE_SIZE_LIMIT_BYTES as u64{
+        return;    
+    }
+
+    if zip_file.is_file() && Path::new(zip_file.name()).extension().unwrap() == "zip" {
+        let mut buf = vec![];
+        match zip_file.read_to_end(&mut buf) {
+            Ok(_) => (),
+            Err(_) => return,
+        }
+        let zip_archive = RefCell::new(ZipArchive::new(Cursor::new(buf)).unwrap());
+        get_file_names_from_zip_archive(zip_archive, mt_d_filenames);
+        
+    } else {
+        let mut buf = vec![];
+        match zip_file.read_to_end(&mut buf) {
+            Ok(_) => (),
+            Err(_) => return,
         };
 
-        let file_names = zip_archive.file_names().map(|s| s.to_string()).collect::<Vec<String>>();
-        for file_name in &file_names {
-            let mut zip_file = match zip_archive.by_name(file_name) {
-                Ok(v) => v,
-                Err(_) => continue,
-            };
-
-            if zip_file.is_dir() {
-                continue;
-            }
-
-            if zip_file.size() >= FILE_SIZE_LIMIT_BYTES as u64{
-                continue;
-            }
-
-            if zip_file.is_file() && Path::new(zip_file.name()).extension().unwrap() == "zip" {
-                let mut buf = vec![];
-                match zip_file.read_to_end(&mut buf) {
-                    Ok(_) => (),
-                    Err(_) => continue
-                };
-                zip_archives_deque.push_back(ZipArchive::new(Cursor::new(buf)).unwrap());
-                continue;
-            }
-
-            let mut buf = vec![];
-            match zip_file.read_to_end(&mut buf) {
-                Ok(_) => (),
-                Err(_) => continue
-            };
-
-            mt_d_file_contents.push_back(Some(String::from_utf8_lossy(&buf).to_string()));
-        }
+        mt_d_file_contents.push_back(Some(String::from_utf8_lossy(&buf).to_string()));
     }
 }
+
+pub fn get_file_names_from_zip_archive(zip_archive: RefCell<ZipArchive<Cursor<Vec<u8>>>>, mt_d_filenames: &MtDeque<Option<FileForIndex>>) {
+    let file_names = zip_archive.borrow_mut().file_names().map(|s| s.to_string()).collect::<Vec<String>>();
+    for file_name in file_names {
+        if Path::new(&file_name).is_dir() {
+            continue;
+        }
+        mt_d_filenames.push_back(Some(FileForIndex::Zip(file_name, RefCell::clone(&zip_archive))));
+    }
+}
+
+pub fn get_file_names_from_zip_path(path: &Path, mt_d_filenames: &MtDeque<Option<FileForIndex>>) {
+    let buf = std::fs::read(path).unwrap();
+    let zip_archive = RefCell::new(ZipArchive::new(Cursor::new(buf)).unwrap());
+    get_file_names_from_zip_archive(zip_archive, mt_d_filenames);
+}
+
--- src/list_and_read.rs
+++ src/list_and_read.rs
@@ -1,72 +1,83 @@
+use std::cell::RefCell;
 use std::fs;
-use std::io::Read;
+use std::io::{Read, Cursor};
 use std::path::PathBuf;
 
+use crate::archives::get_contents_from_zip_file;
 use crate::mt::{size_limits, MtDeque};
 
+#[derive(Debug)]
+pub enum FileForIndex {
+    // Zip(String, RefCell<zip::read::ZipArchive<Cursor<Vec<u8>>>>),
+    Zip(String, RefCell<zip::read::ZipArchive<Cursor<Vec<u8>>>>),
+    Regular(PathBuf),
+}
+
 pub fn read_files_from_deque(
-    mt_d_filenames: &MtDeque<Option<Box<PathBuf>>>,
+    mt_d_filenames: &MtDeque<Option<FileForIndex>>,
     mt_d_file_contents: &MtDeque<Option<String>>,
 ) {
     loop {
-        let file_path = match mt_d_filenames.pop_front() {
+        println!("{:?}", mt_d_filenames.size());
+        let file_for_index = match mt_d_filenames.pop_front() {
             Some(v) => v,
+            // Poison pill for mt_d_filenames
             None => {
                 mt_d_filenames.push_front(None);
                 break;
             }
         };
 
-        if file_path.is_dir() {
-            continue;
-        }
+        match file_for_index {
+            FileForIndex::Regular(file_path) => {
+                if file_path.is_dir() {
+                    continue;
+                }
 
-        let file_ext: String = match file_path.extension() {
-            Some(v) => match v.to_str() {
-                Some(s) => s.to_string(),
-                None => continue,
+                let mut file = match fs::File::open(file_path) {
+                    Ok(v) => v,
+                    Err(_) => continue,
+                };
+
+                if let Ok(file_metadata) = file.metadata() {
+                    if file_metadata.len() >= size_limits::FILE_SIZE_LIMIT_BYTES as u64 {
+                        continue;
+                    }
+                } else {
+                    continue;
+                }
+
+                let mut file_contents = vec![];
+                match file.read_to_end(&mut file_contents) {
+                    Ok(_) => (),
+                    Err(_) => continue,
+                };
+                let file_contents_string = String::from_utf8_lossy(&file_contents).to_string();
+
+                if !file_contents_string.is_empty() {
+                    mt_d_file_contents.push_front(Some(file_contents_string))
+                }
             },
-            None => continue,
-        };
-
-        if file_ext == "zip" {
-            crate::archives::unzip_from_memory(&file_path.as_os_str().to_str().unwrap(), mt_d_file_contents);
-            continue;
-        }
-
-        let mut file = match fs::File::open(file_path.as_ref()) {
-            Ok(v) => v,
-            Err(_) => continue,
-        };
-
-        if let Ok(file_metadata) = file.metadata() {
-            if file_metadata.len() >= size_limits::FILE_SIZE_LIMIT_BYTES as u64 {
-                continue;
+            FileForIndex::Zip(path, zip_archive) => {
+                // println!("{}", path);
+                get_contents_from_zip_file(&path, zip_archive, mt_d_filenames, mt_d_file_contents);
             }
-        } else {
-            continue;
-        }
-
-        let mut file_contents = vec![];
-        match file.read_to_end(&mut file_contents) {
-            Ok(_) => (),
-            Err(_) => continue,
-        };
-        let file_contents_string = String::from_utf8_lossy(&file_contents).to_string();
-
-        if !file_contents_string.is_empty() {
-            mt_d_file_contents.push_front(Some(file_contents_string))
         }
     }
     mt_d_file_contents.push_front(None);
 }
 
-pub fn add_files_to_deque(mt_d_filenames: &MtDeque<Option<Box<PathBuf>>>, indir: &str) {
+pub fn add_files_to_deque(mt_d_filenames: &MtDeque<Option<FileForIndex>>, indir: &str) {
     use walkdir::WalkDir;
 
     for entry in WalkDir::new(indir).into_iter().filter_map(|e| e.ok()) {
-        let path = PathBuf::from(entry.path().as_os_str().to_str().unwrap());
-        mt_d_filenames.push_back(Some(Box::new(path)));
+        // println!("{:?}", entry);
+        if entry.path().is_file() && entry.path().extension().unwrap() == "zip" {
+            crate::archives::get_file_names_from_zip_path(entry.path(), mt_d_filenames);
+        } else if entry.path().is_file() && entry.path().extension().unwrap() == "txt" {
+            let path = PathBuf::from(entry.path().as_os_str().to_str().unwrap());
+            mt_d_filenames.push_back(Some(FileForIndex::Regular(path)));
+        }
     }
-    mt_d_filenames.push_front(None);
+    mt_d_filenames.push_back(None);
 }
